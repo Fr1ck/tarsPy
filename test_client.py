bae2347550352b49d7cd9722dc9f6915cac9c45e
Fr1ck <804763350@qq.com>
2019-07-24 15:50:26 +0800

    epoll

--- test_client.py
+++ test_client.py
@@ -2,20 +2,22 @@
 
 import socket
 
-#创建客户端socket对象
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#服务端IP地址和端口号组员
+
 server_address = ("127.0.0.1", 8888)
-#客户端连接指定的IP地址和端口号
-clientsocket.connect(server_address)
+
+try:
+    clientsocket.connect(server_address)
+    print("connect successful")
+except socket.error:
+    print("fail to connect")
+    quit()
 
 while True:
-    #输入数据
-    data = input('please input:')
-    #客户端发送数据
-    clientsocket.sendall(data.encode())
-    #客户端接收数据
+    data = input('please input: ')
+    clientsocket.send(data.encode())
     server_data = clientsocket.recv(1024).decode()
-    print('客户端收到的数据：', server_data)
-    #关闭客户端socket
-    clientsocket.close()
+
+    print('receive from server：', server_data)
+
+clientsocket.close()
--- test_server.py
+++ test_server.py
@@ -1,95 +1,222 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import select
 import socket
 import queue
+import struct
 
-#创建socket对象
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#设置ip地址复用
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#设置ip地址和端口
-server_address = ("127.0.0.1", 8888)
-#绑定ip地址
-serversocket.bind(server_address)
-#监听，设置最大连接数
-serversocket.listen(10)
-print("监听ip：", server_address)
-
-#设置服务端非阻塞
-serversocket.setblocking(False)
-#超时时间
-timeout = 10
-#创建epoll对象
-epoll = select.epoll()
-#注册服务监听fd带等待读事件集合
-epoll.register(serversocket.fileno(), select.EPOLLIN)
-#创建保存连接客户端信息的字典
+# 存放句柄和socket的字典
+fd_to_socket = {}
+# 存放socket和信息的字典
 message_queues = {}
-#文件句柄到所对应对象的字典，格式为{句柄:对象}
-fd_to_socket = {serversocket.fileno():serversocket, }
 
-while True:
-    print("等待client连接..")
-    # 轮询注册的事件集合，返回值为[(文件句柄，对应的事件)，(...),....]
-    events = epoll.poll(timeout)
-    if not events:
-        print("epoll超时无活动连接，重新轮询...")
-        continue
-    print("有", len(events), "个新事件，开始处理....")
+# 建立serve端接收请求用的socket
+domain = socket.AF_INET
+socket_type = socket.SOCK_STREAM
+_sock = socket.socket(domain, socket_type)
 
-    for fd, event in events:
-        socket = fd_to_socket[fd]
-        # 如果活动socket为当前服务器socket，表示有新连接
-        if socket == serversocket:
-            connection, address = serversocket.accept()
-            print("新连接：", address)
+epoll = select.epoll()
+
+
+class NetThread:
+
+    def __init__(self):
+        pass
+        # 建立用于通知和关闭的socket
+        # _shutdown_sock = socket(domain, socket_type)
+        # _notify_sock = socket(domain, socket_type)
+        #
+        # if not _shutdown_sock:
+        #     print("_shutdown_sock  invalid")
+        #
+        # if not _notify_sock:
+        #     print("_notify_sock invalid")
+
+    def bind(self, ip, port):
+
+        if not _sock:
+            print("bind_sock invalid")
+
+        # 设置ip端口复用
+        _sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_address = (ip, port)
+        try:
+            # 绑定ip地址
+            _sock.bind(server_address)
+        except socket.error:
+            print("bind error")
+        else:
+            print("server already bind fd is ", _sock)
+
+        connection_backlog = 1024
+        try:
+            _sock.listen(connection_backlog)
+        except socket.error:
+            print("listen error")
+
+        try:
+            _sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        except socket.error:
+            print("set keepAlive error")
+
+        try:
+            _sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        except socket.error:
+            print("set TcpNoDelay error")
+
+        try:
+            _sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+        except socket.error:
+            print("set closeWait error")
+
+        try:
+            _sock.setblocking(False)
+        except socket.error:
+            print("set unblock error")
+
+        return _sock
+
+    def create_epoll(self):
+
+        # epoll.register(self._shutdown_sock.fileno(), select.EPOLLIN | select.EPOLLET)
+        # epoll.register(self._notify_sock.fileno(), select.EPOLLIN | select.EPOLLET)
+        epoll.register(_sock.fileno(), select.EPOLLIN | select.EPOLLET)
+
+    def run(self):
+
+        fd_to_socket[_sock.fileno()] = _sock
+
+        while True:
+
+            timeout = 20
+            events = epoll.poll(timeout)
+
+            for fd, event in events:
+
+                ev_socket = fd_to_socket[fd]
+                if ev_socket == _sock:
+                    print("LISTEN")
+                    ret = True
+                    while ret:
+                        ret = self.net_accept()
+
+                elif event & select.EPOLLHUP:
+                    print("EPOLLHUP")
+                    self.net_close(fd)
+
+                elif event & select.EPOLLIN:
+                    if self.net_in(fd, ev_socket):
+                        print("EPOLLIN")
+
+                elif event & select.EPOLLOUT:
+                    self.net_out(fd, ev_socket)
+                    print("EPOLLOUT")
+
+    def net_accept(self):
+
+        try:
+            connection, address = _sock.accept()
+            print("server accept successful address is", address)
+
             # 新连接socket设置为非阻塞
             connection.setblocking(False)
+
             # 注册新连接fd到待读事件集合
             epoll.register(connection.fileno(), select.EPOLLIN)
+
+            try:
+                connection.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            except socket.error:
+                print("set connection keepAlive error")
+
+            try:
+                connection.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            except socket.error:
+                print("set connection TcpNoDelay error")
+
+            try:
+                connection.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+            except socket.error:
+                print("set connection closeWait error")
+
+            try:
+                connection.setblocking(False)
+            except socket.error:
+                print("set connection unblock error")
+
             # 把新连接的文件句柄以及对象保存到字典
             fd_to_socket[connection.fileno()] = connection
-            # 以新连接的对象为键值，值存储在队列中，保存每个连接的信息
-            message_queues[connection] = queue.Queue()
-        # 关闭事件
-        elif event & select.EPOLLHUP:
-            print('client close')
-            # 在epoll中注销客户端的文件句柄
-            epoll.ungregister(fd)
-            # 关闭客户端的文件句柄
-            fd_to_socket[fd].close()
-            # 在字典中删除与已关闭客户端相关的信息
-            del fd_to_socket[fd]
-        # 可读数据
-        elif event & select.EPOLLIN:
-            # 接收数据
-            data = socket.recv(1024).decode()
-            if data:
-                print("收到数据：", data, "客户端：", socket.getpeername())
-                # 将数据放入对应客户端的字典
-                message_queues[socket].put(data)
-                # 修改读取到消息的连接到等待写事件集合(即对应客户端收到消息后，再将其fd修改并加入写事件集合)
-                epoll.modify(fd, select.EPOLLOUT)
-        elif event & select.EPOLLOUT:
-            try:
-                #从字典中获取对应客户端的信息
-                msg = message_queues[socket].get_nowait()
-            except queue.Empty:
-                print(socket.getpeername(), " queue empty")
-                #修改文件句柄为读事件
-                epoll.modify(fd, select.EPOLLIN)
-            else:
-                print("发送数据：", data, "客户端：", socket.getpeername())
-                #发送数据
-                socket.send(msg.encode())
 
-#在epoll中注销服务端文件句柄
-epoll.ungregister(serversocket.fileno())
-#关闭epoll
-epoll.close()
-#关闭服务器socket
-serversocket.close()
+            # 以新连接的对象为键值，之后读取时信息保存每个连接的信息
+            message_queues[connection] = queue.Queue()
+
+            return True
+
+        except socket.error:
+            return False
+
+    def net_close(self, fd):
+
+        epoll.ungregister(fd)
+        # 关闭客户端文件句柄的socket
+        fd_to_socket[fd].close()
+        # 在字典中删除与已关闭客户端相关的信息
+        del fd_to_socket[fd]
+
+    def net_in(self, fd, ev_socket):
+
+        data = ev_socket.recv(1024).decode()
+
+        if data:
+            print("recv：", data, " from：", ev_socket.getpeername())
+            # 将数据放入对应客户端的字典
+            message_queues[ev_socket].put(data)
+            # 修改读取到消息的连接到等待写事件集合(即对应客户端收到消息后，再将其fd修改并加入写事件集合)
+            epoll.modify(fd, select.EPOLLOUT)
+
+            return True
+        else:
+            return False
+
+    def net_out(self, fd, ev_socket):
+
+        try:
+            # 从字典中获取对应客户端的信息
+            msg = message_queues[ev_socket].get_nowait()
+
+        except queue.Empty:
+            print(ev_socket.getpeername(), " queue empty")
+            # 修改文件句柄为读事件
+            epoll.modify(fd, select.EPOLLIN)
+
+        else:
+            print("send msg：", msg, " to：", ev_socket.getpeername())
+            # 发送数据
+            ev_socket.send(msg.encode())
+
+    def __del__(self):
+        # 在epoll中注销服务端文件句柄
+        epoll.unregister(_sock.fileno())
+        # 关闭epoll
+        epoll.close()
+        # 关闭服务器socket
+        _sock.close()
+
+
+if __name__ == '__main__':
+
+    ip = "127.0.0.1"
+    port = 8888
+
+    vNetThread = NetThread()
+    vNetThread.bind(ip, port)
+    vNetThread.create_epoll()
+    vNetThread.run()
+
+    del vNetThread
+
+
 
 
 
